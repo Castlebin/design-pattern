0210cb00220bf29d006a82e5869d100f0d7bce1b
heller <heller@heller.com>
2020-08-02 23:02:15 +0800

    Java 原生的动态代理

--- test/src/main/java/com/heller/dynamic/proxy/TestDynamicProxy.java
+++ test/src/main/java/com/heller/dynamic/proxy/TestDynamicProxy.java
@@ -5,7 +5,7 @@ import java.lang.reflect.Proxy;
 public class TestDynamicProxy {
     public static void main(String[] args) {
         Student student = new Student();
-        People people = (People) Proxy.newProxyInstance(ClassLoader.getSystemClassLoader(),
+        People people = (People) Proxy.newProxyInstance(student.getClass().getClassLoader(),
                 new Class[]{People.class}, new PeopleInvocationHandler(student));
         people.sayHello();;
     }
