408aa17298dc491e1e356a072a35dbd60eee33ae
yuxingao2 <yuxingao2@creditease.cn>
2020-08-04 15:51:08 +0800

    demo

--- asm-basic/src/main/java/com/heller/asm/TestCustomClassWriterMain.java
+++ asm-basic/src/main/java/com/heller/asm/TestCustomClassWriterMain.java
@@ -1,6 +1,6 @@
 package com.heller.asm;
 
-public class Main {
+public class TestCustomClassWriterMain {
     public static void main(String[] args) {
         CustomClassWriter ccw = new CustomClassWriter();
         ccw.publicizeMethod();
