6cab1366b5d38ffe9ab750ecf2f4ba18b4490639
yuxingao2 <yuxingao2@creditease.cn>
2020-08-05 18:07:02 +0800

    利用 Java Attach API 在运行时，动态的修改Java类

--- asm-basic/src/main/java/com/heller/rt/demo/TestAgent.java
+++ asm-basic/src/main/java/com/heller/rt/demo/TestAgent.java
@@ -3,6 +3,10 @@ package com.heller.rt.demo;
 import java.lang.instrument.Instrumentation;
 
 public class TestAgent {
+    /**
+     * 定义Agent，并在其中实现AgentMain方法
+     * 该方法将作为 Agent 的入口
+     */
     public static void agentmain(String args, Instrumentation inst) {
         //指定我们自己定义的Transformer，在其中利用Javassist做字节码替换
         inst.addTransformer(new TestTransformer(), true);
