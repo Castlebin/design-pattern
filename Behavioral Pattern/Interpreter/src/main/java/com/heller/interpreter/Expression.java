483d9859480f864d918b8a5974a6d555b1c1427e
gaoyuxin <gaoyuxin@kuaishou.com>
2021-08-20 20:14:16 +0800

    23种设计模式 Java

--- Behavioral Pattern/Interpreter/src/main/java/com/heller/interpreter/Expression.java
+++
@@ -1,7 +0,0 @@
-package com.heller.interpreter;
-
-public interface Expression {
-
-    float interpret();
-
-}
--- Structural Pattern/flyweight/src/main/java/flyweight/game/Cube.java
+++
@@ -1,7 +0,0 @@
-package flyweight.game;
-
-public class Cube extends _3DObject {
-    public Cube(World world, String name) {
-        super(world, name);
-    }
-}
