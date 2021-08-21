24c0e15d4a54f28701e89e72732c1400c4fa92b6
Castlebin <Castlebin@gmail.com>
2021-08-21 23:51:41 +0800

    继承

---
+++ design-pattern-and-best-practices-in-java/ch01/src/main/java/com/heller/dpj/ch01/Car.java
@@ -0,0 +1,9 @@
+package com.heller.dpj.ch01;
+
+public class Car extends Vehicle {
+
+    public Car(String name) {
+        super(name);
+    }
+
+}
---
+++ design-pattern-and-best-practices-in-java/ch01/src/main/java/com/heller/dpj/ch01/Vehicle.java
@@ -0,0 +1,11 @@
+package com.heller.dpj.ch01;
+
+public class Vehicle {
+
+    private String name;
+
+    public Vehicle(String name) {
+        this.name = name;
+    }
+
+}
