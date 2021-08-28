972a08e65ae255ee1476c411d73986855d61a696
Castlebin <Castlebin@gmail.com>
2021-08-28 10:40:25 +0800

    简单工厂模式：1. 静态工厂模式

---
+++ design-pattern-and-best-practices-in-java/ch02/src/main/java/com/heller/dpj/ch02/simpleFactory/staticFactory/VehicleFactory.java
@@ -0,0 +1,39 @@
+package com.heller.dpj.ch02.simpleFactory.staticFactory;
+
+/**
+ * 简单工厂模式：1. 静态工厂模式
+ */
+public class VehicleFactory {
+    public enum VehicleType {
+        Bike, Car, Truck
+    }
+
+    public static Vehicle create(VehicleType type) {
+        if (type.equals(VehicleType.Bike)) {
+            return new Bike();
+        }
+        if (type.equals(VehicleType.Car)) {
+            return new Car();
+        }
+        if (type.equals(VehicleType.Truck)) {
+            return new Truck();
+        }
+        return null;
+    }
+}
+
+abstract class Vehicle {
+    //abstract Vehicle class
+}
+
+class Bike extends Vehicle {
+    //Bike implementation of vehicle
+}
+
+class Car extends Vehicle {
+    //Car implementation of vehicle
+}
+
+class Truck extends Vehicle {
+    //Bike implementation of vehicle
+}
