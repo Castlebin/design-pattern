27ba986b2a7ed69945b249cd89d661e1e1ed9363
Castlebin <Castlebin@gmail.com>
2023-01-14 17:56:58 +0800

    简单工厂模式 之 静态工厂模式（调用者使用静态工厂来获取所需要的对象实例，而不是自己去new）

--- design-pattern-and-best-practices-in-java/ch02/src/main/java/dpj/ch02/simpleFactory/staticFactory/VehicleFactory.java
+++ design-pattern-and-best-practices-in-java/ch02/src/main/java/dpj/ch02/simpleFactory/staticFactory/VehicleFactory.java
@@ -1,8 +1,9 @@
-package com.heller.dpj.ch02.simpleFactory.staticFactory;
+package dpj.ch02.simpleFactory.staticFactory;
 
 /**
  * 简单工厂模式：1. 静态工厂模式
  */
+// 静态工厂（调用者使用静态工厂来获取所需要的对象实例，而不是自己去new）
 public class VehicleFactory {
     public enum VehicleType {
         Bike, Car, Truck
