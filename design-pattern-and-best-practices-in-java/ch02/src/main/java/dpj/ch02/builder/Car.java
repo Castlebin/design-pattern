5045d058241a694f3fdcd1f133c6f25c07db6391
Castlebin <Castlebin@gmail.com>
2023-01-14 20:33:44 +0800

    建造者模式(构造者模式)  (Builder)

---
+++ design-pattern-and-best-practices-in-java/ch02/src/main/java/dpj/ch02/builder/Car.java
@@ -0,0 +1,5 @@
+package dpj.ch02.builder;
+
+public class Car {
+    // This class has properties of car like color, name, make etc
+}
---
+++ design-pattern-and-best-practices-in-java/ch02/src/main/java/dpj/ch02/builder/CarBuilderDirector.java
@@ -0,0 +1,37 @@
+package dpj.ch02.builder;
+
+// 主类，使用 CarBuilderDirector 来构建对象，而不是直接new
+public class CarBuilderDirector {
+    public Car buildElectricCar(CarBuilder builder) {
+
+        builder.buildCar();
+        builder.addEngine("Electric 150 kW");
+        builder.addBatteries("1500 kWh");
+        builder.addTransmission("Manual");
+        for (int i = 0; i < 4; i++) {
+            builder.addWheel("20x12x30");
+        }
+        builder.paint("red");
+
+        return builder.getCar();
+    }
+
+    public Car buildHybridCar(CarBuilder builder) {
+
+        builder.buildCar();
+        builder.addEngine("Electric 150 kW");
+        builder.addBatteries("1500 kWh");
+        builder.addTransmission("Manual");
+        for (int i = 0; i < 4; i++) {
+            builder.addWheel("20x12x30");
+        }
+        builder.paint("red");
+
+        builder.addGasTank("1500 kWh");
+        builder.addEngine("Gas 1600cc");
+
+        return builder.getCar();
+    }
+
+
+}
