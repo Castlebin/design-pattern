eb0922225f12fc706171fc92792c437e02d6cada
Castlebin <Castlebin@gmail.com>
2023-01-15 19:37:11 +0800

    remove

--- asm-basic/src/main/java/com/heller/asm/demo1/TestCustomClassWriterMain.java
+++
@@ -1,8 +0,0 @@
-package com.heller.asm.demo1;
-
-public class TestCustomClassWriterMain {
-    public static void main(String[] args) {
-        CustomClassWriter ccw = new CustomClassWriter();
-        ccw.publicizeMethod();
-    }
-}
--- asm-basic/src/main/java/com/heller/asm/demo3/Base.java
+++
@@ -1,7 +0,0 @@
-package com.heller.asm.demo3;
-
-public class Base {
-    public void process() {
-        System.out.println("process");
-    }
-}
